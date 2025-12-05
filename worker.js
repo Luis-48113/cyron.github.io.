ab25e691a0546cc15b9b513ab708334b1234884d
Luis-48113 <lgargueta13@icloud.com>
2025-12-04 20:44:53 -0600

    git update worker.js

--- worker.js
+++ worker.js
@@ -43,7 +43,7 @@ export default {
         if (!HF_TOKEN) throw new Error("Hugging Face token not set in environment");
 
         const response = await fetch(
-          "https://api-inference.huggingface.co/v1/models/meta-llama/Llama-3.1-8B-Instruct",
+          "https://huggingface.co/meta-llama/Llama-3.1-8B-Instruct",
           {
             method: "POST",
             headers: {
